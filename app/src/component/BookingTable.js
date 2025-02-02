c439308c95f3aa3b3e7ea771c2dff9d8546bf458
SHuBhAM71205 <84559807+SHuBhAM71205@users.noreply.github.com>
2025-02-02 14:38:23 +0530

    nn

--- app/src/component/BookingTable.js
+++ app/src/component/BookingTable.js
@@ -1,8 +1,34 @@
-
 export default function BookingTable(){
+    const date=new Date;
+
+    const style={
+        "flex-direction":"",
+    }
     return(
-        <>
-        <h1>Hello worold</h1>
-        </>
+        <div className="BookTable flex">
+            <div className="Table"></div>
+            <div className="Tableform">
+                <form>
+                    <fieldset>
+                        <legend>Date-Time</legend>
+                        <input type="date" ></input>
+                        <input type="time"></input>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Table</legend>
+                        
+                    </fieldset>
+                    <fieldset>
+                        <legend>No of People</legend>
+                        
+                    </fieldset>         
+                </form>
+                
+            </div>
+            <div className="conform">mlkm</div>
+        </div>
     );
 }
+
+
+// `${date.getDate}+${date.getMonth}+${date.getFullYear}`
